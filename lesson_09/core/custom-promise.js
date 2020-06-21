3c8994354395c10938d17851edc421f7b93f6b8d
avbelyshev <avbelishev@gmail.com>
2020-06-21 19:00:01 +0300

    CustomPromise status & static methods

--- lesson_09/core/custom-promise.js
+++ lesson_09/core/custom-promise.js
@@ -3,6 +3,8 @@ class CustomPromise {
         this.__success__ = [];
         this.__error__ = [];
         this._callback = callback;
+        this._status = 'pending';
+        this._value = null;
 
         setTimeout( () => {
             callback(this._resolve.bind(this), this._reject.bind(this));
@@ -22,11 +24,31 @@ class CustomPromise {
         this.then(null, rejectCb);
     }
 
-    _resolve(result) {
-        this.__success__.forEach( cb => cb(result) );
+    static resolve(value) {
+        return new CustomPromise((resolve, reject) => {
+            resolve(value);
+        });
     }
 
-    _reject(err) {
-        this.__error__.forEach( cb => cb(err) );
+    static reject(value) {
+        return new CustomPromise((resolve, reject) => {
+            reject(value);
+        });
+    }
+
+    _resolve(value) {
+        this.__success__.forEach( cb => cb(value) );
+        if (this._status === 'pending') {
+            this._status = 'fulfilled';
+            this._value = value;
+        }
+    }
+
+    _reject(value) {
+        this.__error__.forEach( cb => cb(value) );
+        if (this._status === 'pending') {
+            this._status = 'rejected';
+            this._value = value;
+        }
     }
 }
--- lesson_09/script.js
+++ lesson_09/script.js
@@ -4,7 +4,17 @@ var promise = new CustomPromise((resolve, reject) => {
     }, 1000);
 });
 
-promise.then(console.log);
+promise.then((result) => {
+    console.log('Resolved', result);
+});
 promise.catch((err) => {
     console.log('Rejected', err);
 });
+promise.catch((err) => {
+    console.log('Rejected2', err);
+});
+
+console.log(promise);
+
+var promise2 = CustomPromise.resolve(1);
+console.log(promise2);
